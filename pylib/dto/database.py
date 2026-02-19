425896a5c0d33cc89d351e18b2fd205fd2eeed2f
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2026-02-18 23:35:15 -0300

    dev: new database schema with batches

--- pylib/dto/database.py
+++ pylib/dto/database.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from bson import ObjectId
  
 from .simulator import SimulationConfig 
-    
+
 #---------------------------------------------------------------------------------------------------------    
 # Database Structure -------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------
@@ -11,17 +11,17 @@ from .simulator import SimulationConfig
 class SourceFile(TypedDict):
     id: str
     file_name: str
-    
+
 class SourceRepository(TypedDict):
     id: str
     name : str
     description: str
     source_files: list[SourceFile]
-    
+
 class Simulation(TypedDict):
     id: str
     experiment_id: ObjectId
-    generation_id: ObjectId
+    batch_id: ObjectId
     status: str
     random_seed: int
     start_time: datetime
@@ -34,26 +34,24 @@ class Simulation(TypedDict):
     runtime_log_id: ObjectId
     csv_log_id: ObjectId
     topology_picture_id: ObjectId
-    objectives: dict[str,float]
-    metrics: dict[str,float]
-    
+    network_metrics: dict[str,float]
+
+class Individual(TypedDict):
+    chromosome: dict[str, Any]
+    objectives: list[float]
+    topology_picture_id: ObjectId
+    simulations_ids: list[ObjectId]
+
 class Generation(TypedDict):
-    id: str
     index: int
-    experiment_id: ObjectId
+    population: list[Individual]
+
+class Batch(TypedDict):
+    _id: ObjectId
     status: str
     start_time: datetime
     end_time: datetime
     simulations_ids: list[ObjectId]
-
-class ObjectiveItem(TypedDict):
-    name: str
-    kind: str
-    column: str
-    goal: str
-    # Optional parameters (validated according to the type)
-    q: Optional[float] = None         # required if kind == QUANTILE (0<q<=1)
-    scale: Optional[float] = None     # required if kind == INVERSE_MEDIAN (scale>0)
 
 class MetricItem(TypedDict):
     name: str
@@ -61,11 +59,15 @@ class MetricItem(TypedDict):
     column: str
     # Optional parameters (validated according to the type)
     q: Optional[float] = None         # required if kind == QUANTILE (0<q<=1)
+    scale: Optional[float] = None     # required if kind == INVERSE_MEDIAN (scale>0)
 
-class TransformConfig(TypedDict):
+class ObjetiveItem(TypedDict):
+    metric_name: str
+    goal: str # "min" or "max"
+    
+class DataConversionConfig(TypedDict):
     node_col: str
     time_col: str
-    objectives: list[ObjectiveItem]
     metrics: list[MetricItem]
       
 class Parameters(TypedDict):
@@ -73,9 +75,9 @@ class Parameters(TypedDict):
     algorithm: dict[str, Any] # Algorithm parameters defined in dto/algorithms.py
     simulation: dict[str, Any] # This is not Simulation or SimulationConfig because it holds only the simulation parameters
     problem: dict[str, Any] # Problem parameters defined in dto/problems.py
+    objectives: list[ObjetiveItem]
     
 class ParetoFrontItem(TypedDict):
-    simulation_id: ObjectId
     chromosome: dict[str, Any]
     objectives: dict[str, float]    
 
@@ -88,8 +90,8 @@ class Experiment(TypedDict):
     start_time: datetime
     end_time: datetime
     parameters: Parameters
-    generations: list[ObjectId]
+    generations: list[Generation]
     source_repository_options: dict[str, ObjectId]
-    transform_config: TransformConfig
+    data_conversion_config: DataConversionConfig
     pareto_front: Optional[list[ParetoFrontItem]] = None
     analysis_files: dict[str, ObjectId]
