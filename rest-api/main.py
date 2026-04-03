8c02176836318934713f7b9ff26d02f754a0c5a8
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2026-04-03 17:19:32 -0300

    update: cors adjustment in the api

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import os, sys
-project_path = os.path.abspath(os.path.join(os.getcwd(), "..")) 
+project_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
 if project_path not in sys.path:
     sys.path.insert(0, project_path)
-    
+
 from api.router import api_router
 
 app = FastAPI(
@@ -12,6 +13,15 @@ app = FastAPI(
     version="2.0.0",
     openapi_version="3.0.3"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == "__main__":
