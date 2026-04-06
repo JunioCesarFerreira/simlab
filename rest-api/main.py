b788e55a1bed6289e7af791997767dda80c813c0
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2026-04-05 22:58:40 -0300

    feat: add endpoint helthcheck rest api

--- rest-api/main.py
+++ rest-api/main.py
@@ -24,6 +24,10 @@ app.add_middleware(
 
 app.include_router(api_router, prefix="/api/v1")
 
+@app.get("/health", tags=["health"], include_in_schema=False)
+def health_check():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
